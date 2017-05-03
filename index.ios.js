6cde854e6a246d90f1a929507df74bcb1df6efcb
Joaquin Fernandez <joaquin@asperasoft.com>
2017-05-03 05:40:36 +0200

    Add Appodeal for ios

--- index.ios.js
+++ index.ios.js
@@ -4,13 +4,17 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import React, { Component } from 'react';
+ import {
+     AppRegistry,
+     StyleSheet,
+     Text,
+     TouchableHighlight,
+     View,
+     NativeModules, //to access plugin
+     NativeAppEventEmitter, //callbacks
+     requireNativeComponent
+ } from 'react-native';
 
 export default class DeportesMadrid extends Component {
   render() {
@@ -30,6 +34,22 @@ export default class DeportesMadrid extends Component {
     );
   }
 }
+
+var Appodeal = NativeModules.AppodealPlugin; //appodeal plugin
+var NativeAdViewManagerPlugin = NativeModules.AppodealNativeAdViewManager; //native ad plugin
+var AppodealNativeAdView; //native ad view to render
+var INTERSTITIAL = 1;
+var SKIPPABLE_VIDEO = 2;
+var BANNER = 4;
+var BANNER_BOTTOM = 8;
+var BANNER_TOP = 16;
+var NATIVE_AD = 32;
+var REWARDED_VIDEO = 128;
+var NON_SKIPPABLE_VIDEO = 256;
+
+Appodeal.initializeWithApiKey('8e485ea405f5f8e3f4f0814eb93c0cc65ce3d4f20465ef12', INTERSTITIAL + BANNER);
+Appodeal.setTesting(true);
+Appodeal.show(BANNER_BOTTOM, (result) => { console.log(result);});
 
 const styles = StyleSheet.create({
   container: {
---
+++ ios/AppodealNativeAdViewManager.m
@@ -0,0 +1,1 @@
+../../Appodeal/AppodealNativeAdViewManager.m/
