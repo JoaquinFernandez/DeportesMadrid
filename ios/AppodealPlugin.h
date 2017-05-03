5f0a5e7e1927d18af63d6fd599929c5a68b17004
Joaquin Fernandez <joaquin@asperasoft.com>
2017-05-03 06:03:47 +0200

    Fix Appodeal imports

--- ios/AppodealPlugin.h
+++ ios/AppodealPlugin.h
@@ -1,1 +1,17 @@
-../../Appodeal/AppodealPlugin.h/
+//
+//  AppodealPlugin.h
+//
+//
+//  Created by Denis on 26.05.16.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import "React/RCTBridge.h"
+#import "React/RCTEventDispatcher.h"
+#import <Appodeal/Appodeal.h>
+
+@interface AppodealPlugin : NSObject <RCTBridgeModule, AppodealBannerDelegate, AppodealInterstitialDelegate, AppodealRewardedVideoDelegate, AppodealNonSkippableVideoDelegate, AppodealSkippableVideoDelegate>
+
+@end
